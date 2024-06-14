091748c13a6a84b3b6d05bc05038abae49001ac8
ivan-note <meyerivanoliver@gmail.com>
2024-06-14 17:20:15 -0300

    se agrego la ecuación y la grafica

--- integrador.py
+++ integrador.py
@@ -1,4 +1,6 @@
 import os
+import formula
+
 
 def limpiar_pantalla() -> None:
     if os.name == 'posix':
@@ -8,13 +10,14 @@ def limpiar_pantalla() -> None:
 
 
 def main() -> None:
+    ecuacion = formula.Ecuacion(0.1, 50)
     while True:
         print("Elija una opción: ")
         ans = int(input("(1) Mostrar datos calculados\n(2) Ingresar una coordenada para el cálculo de error\n(3) Mostrar historial de coordenadas y error\n(4) Buscar un dato en la lista ingresando el tiempo\n(5) Buscar un dato en la lista ingresando la variable dependiente o su valor más cercano\n(6) Salir del programa\n"))
         match ans:
             case 1: 
                 limpiar_pantalla()
-                print("1")
+                ecuacion.mostrar_datos()
             case 2:
                 limpiar_pantalla()
                 print("2")
